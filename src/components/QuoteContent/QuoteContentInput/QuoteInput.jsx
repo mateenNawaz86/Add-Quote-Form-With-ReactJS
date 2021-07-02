9b1f186b87acb77e8659747b22a5d6fb1a384c13
Mateen Mirani <mateenmirani125@gmail.com>
2021-07-02 22:17:38 +0500

    Add React Basic files

--- src/components/QuoteContent/QuoteContentInput/QuoteInput.jsx
+++ src/components/QuoteContent/QuoteContentInput/QuoteInput.jsx
@@ -8,7 +8,7 @@ const QuoteInput = (props) => {
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
-    if(event.target.value.trim().length > 0) {
+    if (event.target.value.trim().length > 0) {
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
@@ -21,16 +21,21 @@ const QuoteInput = (props) => {
       return;
     }
     props.onAddQuote(enteredValue);
+    setEnteredValue("");
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={`form-control ${!isValid ? "invalid" : ""}`}>
         <label>Enter New Quotes</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          type="text"
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Quote</Button>
-      <p id='caption'>Mirani ❤️</p>
+      <p id="caption">Mirani ❤️</p>
     </form>
   );
 };
